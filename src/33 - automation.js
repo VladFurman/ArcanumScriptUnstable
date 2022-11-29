3ca318cab9539f889d61b3891b3bc7af4ba2381b
VladFurman <vfurman@ucsc.edu>
2022-11-29 03:10:57 -0800

    Start adding automation.

---
+++ src/33 - automation.js
@@ -0,0 +1,21 @@
+// Automation
+(function($) {
+    'use strict';
+
+    var tc_debug = false;	// set to true to see debug messages
+
+    var tc_suspend = false;		// set this to true in console to suspend all auto functions
+
+    // Setting to false will stop individual actions
+    var tc_auto_misc = true;
+    var tc_use_sublimate = true;
+    var tc_auto_gather = true;
+    var tc_auto_grind = true;
+    var tc_auto_cast = true;
+    var tc_skipcast = true;
+    var tc_auto_focus = true;
+    var tc_auto_earn_gold = false;
+    var tc_auto_heal = true;
+    var tc_auto_adv = true;
+    var tc_auto_focus_aggressive = false;
+})(window.jQuery);
